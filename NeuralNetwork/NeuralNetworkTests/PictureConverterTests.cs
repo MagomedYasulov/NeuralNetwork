fff70edb2dea470e7b126aed51a9cb67c275d3bd
magomed <magomedyasulov8@gmail.com>
2024-04-13 16:05:28 +0300

    Добавил pictureConverter и тесты для него

---
+++ NeuralNetwork/NeuralNetworkTests/PictureConverterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralNetwork.Helpers;
+
+namespace NeuralNetworkTests
+{
+    [TestClass]
+    public class PictureConverterTests
+    {
+        [TestMethod()]
+        public void ConvertTest()
+        {
+            var converter = new PictureConverter();
+            var parasitized = converter.Convert("Images\\Parasitized.png");
+            var unParasitized = converter.Convert("Images\\Unparasitized.png");
+
+            converter.Save("Images\\ConvertedParasitied.png", parasitized);
+            converter.Save("Images\\ConvertedUnparasitied.png", unParasitized);
+        }
+    }
+}
